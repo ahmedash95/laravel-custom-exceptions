1a3f87e74252475b900b54dd9f1834e60ba65ede
Ahmed Ashraf <ahmed29329@gmail.com>
2021-03-30 21:40:02 +0200

    custom message

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Services\ProductService;
+use App\Exceptions\ProductNotFoundException;
 
 class ProductsController extends Controller
 {
     public function show($id, ProductService $productService)
     {
-        $product = $productService->find($id);
+    	try {
+        	$product = $productService->find($id);
+    	} catch (ProductNotFoundException $e) {
+    		return 'Talk to customer support!';
+    	}
 
         return 'Product title: '.$product->getTitle();
     }
